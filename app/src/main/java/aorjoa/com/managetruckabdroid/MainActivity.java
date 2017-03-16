f5b11321363e73a2bfd636af14318358c1b39579
aorjoa <dekcomgang@hotmail.com>
2017-03-16 18:16:34 +0700

    can show all record and override back button

--- app/src/main/java/aorjoa/com/managetruckabdroid/MainActivity.java
+++ app/src/main/java/aorjoa/com/managetruckabdroid/MainActivity.java
@@ -1,7 +1,9 @@
 package aorjoa.com.managetruckabdroid;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
@@ -65,14 +67,55 @@ public class MainActivity extends AppCompatActivity {
 
     public void gotoAddNewRecord(View view){
         setContentView(R.layout.add_new_record);
-        final Button saveAddRecord = (Button) findViewById(R.id.savAddRecord);
+        final Button calPrice = (Button) findViewById(R.id.calPrice);
+        calPrice.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    Record record = createRecord();
+                    Double hvArea = Double.parseDouble(record.getHvArea());
+                    Double hvAreaPrice = Double.parseDouble(record.getHvPriceArea());
+                    Double bhHour = Double.parseDouble(record.getBhHours());
+                    Double bhPriceHour = Double.parseDouble(record.getBhPriceHours());
+                    Double trNum = Double.parseDouble(record.getTrNum());
+                    Double trPriceNum = Double.parseDouble(record.getTrPriceNum());
+                    Double ttNum = Double.parseDouble(record.getTtNum());
+                    Double ttPriceNum = Double.parseDouble(record.getTtPriceNum());
+                    EditText edt = (EditText)findViewById(R.id.prices);
+                    edt.setText(""+(hvArea*hvAreaPrice + bhHour*bhPriceHour + trNum*trPriceNum + ttNum*ttPriceNum));
+
+                }catch (Exception e){
+                    alertMsg("กรุณากรอบข้อมูลให้ถูกต้อง");
+                }
+            }
+        });
+        final Button saveAddRecord = (Button) findViewById(R.id.saveAddRecord);
         saveAddRecord.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                dbSqlite.addRecordToDb(createRecord());
+                try {
+                    dbSqlite.addRecordToDb(createRecord());
+                    alertMsg("บันทึกข้อมูลเรียบร้อยแล้ว");
+                    createAfterAddRecord();
+                }catch (Exception e){
+                    alertMsg("พบข้อผิดพลาด กรุณาตรวจสอบข้อมูลให้ถูกต้อง");
+                }
             }
         });
     }
 
+    public void alertMsg(String msg){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+        dialog.setCancelable(false);
+        dialog.setTitle("โปรดทราบ");
+        dialog.setMessage(msg);
+        dialog.setPositiveButton("เข้าใจแล้ว", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+
+        final AlertDialog alert = dialog.create();
+        alert.show();
+    }
 
     // 2.0 and above
     @Override
@@ -128,8 +171,50 @@ public class MainActivity extends AppCompatActivity {
         String ttNum = edt.getText().toString();
         edt = (EditText)findViewById(R.id.ttPriceNum);
         String ttPriceNum = edt.getText().toString();
-        edt = (EditText)findViewById(R.id.price);
+        edt = (EditText)findViewById(R.id.prices);
         String price = edt.getText().toString();
         return new Record(ctId,ctName,ctPhone,ctAddress,dateOp,hvName,hvArea,hvPriceArea,hvAddress,bhName,bhHours,bhPriceHours,trName,trNum,trPriceNum,ttName,ttNum,ttPriceNum,price,"admin");
+    }
+
+    public void createAfterAddRecord(){
+        EditText edt = null;
+        edt = (EditText)findViewById(R.id.ctId);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.ctName);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.ctPhone);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.ctAddress);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.dateOp);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.hvName);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.hvArea);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.hvPriceArea);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.hvAddress);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.bhName);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.bhHours);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.bhPriceHours);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.trName);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.trNum);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.trPriceNum);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.ttName);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.ttNum);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.ttPriceNum);
+        edt.setText("");
+        edt = (EditText)findViewById(R.id.prices);
+        edt.setText("");
     }
 }
